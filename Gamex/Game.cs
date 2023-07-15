54b40bae8cb498881dab13aa1dda9c60beec626d
Yury Rivera <yrivera@applaudostudios.com>
2023-07-14 23:45:52 -0600

    firsts commit

---
+++ Gamex/Game.cs
@@ -0,0 +1,23 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Gamex;
+
+public class Game : GameWindow
+{
+    public Game(int width, int height, string title) : base(GameWindowSettings.Default,
+        new NativeWindowSettings { Size = (width, height), Title = title })
+    {
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+        base.OnUpdateFrame(args);
+
+        if (KeyboardState.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+    }
+}
---
+++ Gamex/Program.cs
@@ -0,0 +1,4 @@
+﻿using Gamex;
+
+using Game game = new(800, 600, "Gamex");
+game.Run();
